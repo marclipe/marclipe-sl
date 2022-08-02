e3998de550bb35ce1b9095100e6f30629e39c177
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-08-02 15:42:05 -0300

    Ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,66 @@ const showMenu = (toggleId, navId) => {
 showMenu('nav-toggle', 'nav-menu'); 
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav_link');
+const navLink = document.querySelectorAll('.nav_link')
 
-function linkAction() {
-    //Active Link
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active'); 
-
-    //Remove menu mobile
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show'); 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+/*A importação já foi feita*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+/*===== SCROLL HOME =====*/
+sr.reveal('.home_title', {}); 
+sr.reveal('.button', {delay: 200});
+sr.reveal('.home_img', {delay: 400}); 
+sr.reveal('.home_social-icon', {interval: 200}); 
+
+/*===== SCROLL ABOUT =====*/
+sr.reveal('.about_img',{})
+sr.reveal('.about_subtitle',{delay: 200})
+sr.reveal('.about_text',{delay: 400})
+
+/*===== SCROLL SKILLS =====*/
+sr.reveal('.skills_subtitle',{})
+sr.reveal('.skills_text',{delay: 200})
+sr.reveal('.skills_data',{interval: 200})
+sr.reveal('.skills_img',{delay: 400})
+
+/*===== SCROLL WORK =====*/
+sr.reveal('.work_img',{interval: 200})
+
+/*===== SCROLL CONTACT =====*/
+sr.reveal('.contact_input',{interval: 200})
+
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
